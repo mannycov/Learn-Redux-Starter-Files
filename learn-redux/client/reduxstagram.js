dfe73a2e64fd0daf4e8c5c50294f8708ab57ab80
Emanuel <manny.cov09@gmail.com>
2017-12-19 19:47:49 -0800

    add react router and pass down photogrid/single child components as children

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,23 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/style.styl';
+
+import Main from './components/Main.jsx';
+import PhotoGrid from './components/PhotoGrid.jsx';
+import Single from './components/Single.jsx';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'; 
+
+const Root = () => (
+  <div>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </div>
+);
+
+render(<Root />, document.getElementById('root'));
