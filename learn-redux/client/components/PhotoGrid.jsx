e30095d4618fa5fd812673283017b993467107d9
Emanuel <manny.cov09@gmail.com>
2017-12-24 19:26:28 -0800

    implement increment likes reducer

--- learn-redux/client/components/PhotoGrid.jsx
+++ learn-redux/client/components/PhotoGrid.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Photo from './Photo.jsx';
 
 class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        PhotoGrid
+        {this.props.posts.map((post, i) => 
+          <Photo 
+            {...this.props}
+            key={i}
+            i={i}
+            post={post}
+          />
+        )}
       </div>
     )
   }
