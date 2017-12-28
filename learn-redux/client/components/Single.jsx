0ff6cfad6eb782f2536fe229cea628cd675d3b52
Emanuel <manny.cov09@gmail.com>
2017-12-28 15:05:36 -0800

    add postcomment and removecomment functions

--- learn-redux/client/components/Single.jsx
+++ learn-redux/client/components/Single.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import Photo from './Photo.jsx';
+import Comments from './Comments.jsx';
 
 class Single extends Component {
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-        Single Comp
+        <Photo 
+          i={i}
+          post={post}
+          {...this.props}
+        />
+        <Comments 
+          postComments={postComments} 
+          {...this.props}
+        />
       </div>
     )
   }
